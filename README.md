0487a2aac666239dec1db21019daf4bbcce1477c
rok93 <goodboy302@naver.com>
2021-07-06 01:01:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Playground ⚽️ - 로키점 
+
+> 1호점은 마크의 [Spring-learning-sandbox, 스프링 놀이터](https://github.com/binghe819/spring-learning-sandbox) 입니다
---
+++ src/main/java/com/example/playgroundroki/PlaygroundRokiApplication.java
@@ -0,0 +1,13 @@
+package com.example.playgroundroki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundRokiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundRokiApplication.class, args);
+    }
+
+}
